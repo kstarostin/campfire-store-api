dff108d26c9b11ff0c5c6bcf7de7f249e5a5404c
kstarostin <k.e.starostin@gmail.com>
2024-04-17 15:26:04 +0200

    Added localized product description, updated test data and the postman collection

--- models/productModel.js
+++ models/productModel.js
@@ -1,9 +1,58 @@
 const mongoose = require('mongoose');
 const slugifyName = require('../utils/slugifyName');
-const { allowedCurrencies } = require('../utils/config');
+const { allowedCurrencies, allowedLanguages } = require('../utils/config');
 
 /**
- * Describes the complete schema of the product model type.
+ * PRICE SCHEMA
+ */
+const priceSchema = new mongoose.Schema({
+  value: {
+    type: Number,
+    required: [true, 'Price must have a value'],
+  },
+  currency: {
+    type: String,
+    required: [true, 'Price must have a currency'],
+    enum: {
+      values: allowedCurrencies,
+      message: `Allowed Currencies are [${allowedCurrencies.join(', ')}]`,
+    },
+  },
+});
+
+/**
+ * LOCALIZED TEXT VALUE SCHEMA
+ */
+const i18nValueSchema = new mongoose.Schema({
+  lang: {
+    type: String,
+    required: [true, 'Localized text must have a language specified'],
+    enum: {
+      values: allowedLanguages,
+      message: `Allowed text languages are [${allowedLanguages.join(', ')}]`,
+    },
+  },
+  value: {
+    type: String,
+    required: [true, 'Localized text must have a value specified'],
+  },
+});
+
+/**
+ * LOCALIZED TEXT SCHEMA
+ */
+const i18nTextSchema = new mongoose.Schema({
+  values: {
+    type: [i18nValueSchema],
+    validate: [
+      (el) => Array.isArray(el) && el.length > 0,
+      'Please add at least one localized value',
+    ],
+  },
+});
+
+/**
+ * PRODUCT SCHEMA
  */
 const productSchema = new mongoose.Schema(
   {
@@ -22,20 +71,11 @@ const productSchema = new mongoose.Schema(
       required: [true, 'Product must have a name'],
       trim: true,
     },
+    description: i18nTextSchema,
     slug: String,
     price: {
-      value: {
-        type: Number,
-        required: [true, 'Product price must have a value'],
-      },
-      currency: {
-        type: String,
-        required: [true, 'Product price must have a currency'],
-        enum: {
-          values: allowedCurrencies,
-          message: `Allowed price currencies are [${allowedCurrencies.join(', ')}]`,
-        },
-      },
+      type: priceSchema,
+      required: true,
     },
     manufacturer: {
       type: String,
