3a40d4cde18629da5076de88c3f8e2f76c601d6f
kstarostin <k.e.starostin@gmail.com>
2024-04-25 12:54:16 +0200

    Updated postman dev environment and swagger main description

--- swagger/swaggerConfig.js
+++ swagger/swaggerConfig.js
@@ -18,7 +18,7 @@ const document = swaggerJSDoc({
     openapi: '3.0.0',
     info: {
       title: 'Campfire Store API',
-      description: `API for Campfire online shop. It provides access to the shop's products, category system and simple user management, allows you to add products to the cart and place orders.<br><br>Some resources are protected and require prior authorization.`,
+      description: `API for Campfire online shop. It provides access to the shop's products, category system and simple user management, allows you to add products to the cart and place orders.<br><br>Protected resources require prior authorization. Some resources are restricted to users without an admin role.`,
       version: `${packageJson.version}`,
     },
     servers: [{ url: apiPath }],
